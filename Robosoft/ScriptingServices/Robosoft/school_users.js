21b892eda493c2b4c0675873a747fa636ad7135a
valchovski <bvalchovski@gmail.com>
2015-06-27 10:39:59 +0000

    Initial UI and backend for Principal

---
+++ Robosoft/ScriptingServices/Robosoft/school_users.js
@@ -0,0 +1,3 @@
+var entitySchool_users = require('Robosoft/school_users_lib');
+
+entitySchool_users.processSchool_users();
---
+++ Robosoft/ScriptingServices/Robosoft/school_users_lib.js
@@ -0,0 +1,289 @@
+var systemLib = require('system');
+var ioLib = require('io');
+var entityLib = require('entity');
+
+// create entity by parsing JSON object from request body
+exports.createSchool_users = function() {
+    var input = ioLib.read(request.getReader());
+    var message = JSON.parse(input);
+    var connection = datasource.getConnection();
+    try {
+        var sql = "INSERT INTO SCHOOL_USERS (";
+        sql += "ID";
+        sql += ",";
+        sql += "USERNAME";
+        sql += ",";
+        sql += "USER_TYPE";
+        sql += ") VALUES ("; 
+        sql += "?";
+        sql += ",";
+        sql += "?";
+        sql += ",";
+        sql += "?";
+        sql += ")";
+
+        var statement = connection.prepareStatement(sql);
+        var i = 0;
+        var id = db.getNext('SCHOOL_USERS_ID');
+        statement.setInt(++i, id);
+        statement.setString(++i, message.username);
+        statement.setInt(++i, message.user_type);
+        statement.executeUpdate();
+        response.getWriter().println(id);
+        return id;
+    } catch(e) {
+        var errorCode = javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+        entityLib.printError(errorCode, errorCode, e.message);
+    } finally {
+        connection.close();
+    }
+    return -1;
+};
+
+// read single entity by id and print as JSON object to response
+exports.readSchool_usersEntity = function(id) {
+    var connection = datasource.getConnection();
+    try {
+        var result = "";
+        var sql = "SELECT * FROM SCHOOL_USERS WHERE " + pkToSQL();
+        var statement = connection.prepareStatement(sql);
+        statement.setString(1, id);
+        
+        var resultSet = statement.executeQuery();
+        var value;
+        while (resultSet.next()) {
+            result = createEntity(resultSet);
+        }
+        if(result.length === 0){
+            entityLib.printError(javax.servlet.http.HttpServletResponse.SC_NOT_FOUND, 1, "Record with id: " + id + " does not exist.");
+        }
+        var text = JSON.stringify(result, null, 2);
+        response.getWriter().println(text);
+    } catch(e){
+        var errorCode = javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+        entityLib.printError(errorCode, errorCode, e.message);
+    } finally {
+        connection.close();
+    }
+};
+
+// read all entities and print them as JSON array to response
+exports.readSchool_usersList = function(limit, offset, sort, desc) {
+    var connection = datasource.getConnection();
+    try {
+        var result = [];
+        var sql = "SELECT ";
+        if (limit !== null && offset !== null) {
+            sql += " " + db.createTopAndStart(limit, offset);
+        }
+        sql += " * FROM SCHOOL_USERS";
+        if (sort !== null) {
+            sql += " ORDER BY " + sort;
+        }
+        if (sort !== null && desc !== null) {
+            sql += " DESC ";
+        }
+        if (limit !== null && offset !== null) {
+            sql += " " + db.createLimitAndOffset(limit, offset);
+        }
+        var statement = connection.prepareStatement(sql);
+        var resultSet = statement.executeQuery();
+        var value;
+        while (resultSet.next()) {
+            result.push(createEntity(resultSet));
+        }
+        var text = JSON.stringify(result, null, 2);
+        response.getWriter().println(text);
+    } catch(e){
+        var errorCode = javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+        entityLib.printError(errorCode, errorCode, e.message);
+    } finally {
+        connection.close();
+    }
+};
+
+//create entity as JSON object from ResultSet current Row
+function createEntity(resultSet, data) {
+    var result = {};
+	result.id = resultSet.getInt("ID");
+    result.username = resultSet.getString("USERNAME");
+	result.user_type = resultSet.getInt("USER_TYPE");
+    return result;
+}
+
+function convertToDateString(date) {
+    var fullYear = date.getFullYear();
+    var month = date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth();
+    var dateOfMonth = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    return fullYear + "/" + month + "/" + dateOfMonth;
+}
+
+// update entity by id
+exports.updateSchool_users = function() {
+    var input = ioLib.read(request.getReader());
+    var message = JSON.parse(input);
+    var connection = datasource.getConnection();
+    try {
+        var sql = "UPDATE SCHOOL_USERS SET ";
+        sql += "USERNAME = ?";
+        sql += ",";
+        sql += "USER_TYPE = ?";
+        sql += " WHERE ID = ?";
+        var statement = connection.prepareStatement(sql);
+        var i = 0;
+        statement.setString(++i, message.username);
+        statement.setInt(++i, message.user_type);
+        var id = "";
+        id = message.id;
+        statement.setInt(++i, id);
+        statement.executeUpdate();
+        response.getWriter().println(id);
+    } catch(e){
+        var errorCode = javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+        entityLib.printError(errorCode, errorCode, e.message);
+    } finally {
+        connection.close();
+    }
+};
+
+// delete entity
+exports.deleteSchool_users = function(id) {
+    var connection = datasource.getConnection();
+    try {
+        var sql = "DELETE FROM SCHOOL_USERS WHERE "+pkToSQL();
+        var statement = connection.prepareStatement(sql);
+        statement.setString(1, id);
+        var resultSet = statement.executeUpdate();
+        response.getWriter().println(id);
+    } catch(e){
+        var errorCode = javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+        entityLib.printError(errorCode, errorCode, e.message);
+    } finally {
+        connection.close();
+    }
+};
+
+exports.countSchool_users = function() {
+    var count = 0;
+    var connection = datasource.getConnection();
+    try {
+        var statement = connection.createStatement();
+        var rs = statement.executeQuery('SELECT COUNT(*) FROM SCHOOL_USERS');
+        while (rs.next()) {
+            count = rs.getInt(1);
+        }
+    } catch(e){
+        var errorCode = javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+        entityLib.printError(errorCode, errorCode, e.message);
+    } finally {
+        connection.close();
+    }
+    response.getWriter().println(count);
+};
+
+exports.metadataSchool_users = function() {
+	var entityMetadata = {};
+	entityMetadata.name = 'school_users';
+	entityMetadata.type = 'object';
+	entityMetadata.properties = [];
+	
+	var propertyid = {};
+	propertyid.name = 'id';
+	propertyid.type = 'integer';
+	propertyid.key = 'true';
+	propertyid.required = 'true';
+    entityMetadata.properties.push(propertyid);
+
+	var propertyusername = {};
+	propertyusername.name = 'username';
+    propertyusername.type = 'string';
+    entityMetadata.properties.push(propertyusername);
+
+	var propertyuser_type = {};
+	propertyuser_type.name = 'user_type';
+	propertyuser_type.type = 'integer';
+    entityMetadata.properties.push(propertyuser_type);
+
+
+    response.getWriter().println(JSON.stringify(entityMetadata));
+};
+
+function getPrimaryKeys(){
+    var result = [];
+    var i = 0;
+    result[i++] = 'ID';
+    if (result === 0) {
+        throw new Exception("There is no primary key");
+    } else if(result.length > 1) {
+        throw new Exception("More than one Primary Key is not supported.");
+    }
+    return result;
+}
+
+function getPrimaryKey(){
+	return getPrimaryKeys()[0].toLowerCase();
+}
+
+function pkToSQL(){
+    var pks = getPrimaryKeys();
+    return pks[0] + " = ?";
+}
+
+exports.processSchool_users = function() {
+	
+	// get method type
+	var method = request.getMethod();
+	method = method.toUpperCase();
+	
+	//get primary keys (one primary key is supported!)
+	var idParameter = getPrimaryKey();
+	
+	// retrieve the id as parameter if exist 
+	var id = xss.escapeSql(request.getParameter(idParameter));
+	var count = xss.escapeSql(request.getParameter('count'));
+	var metadata = xss.escapeSql(request.getParameter('metadata'));
+	var sort = xss.escapeSql(request.getParameter('sort'));
+	var limit = xss.escapeSql(request.getParameter('limit'));
+	var offset = xss.escapeSql(request.getParameter('offset'));
+	var desc = xss.escapeSql(request.getParameter('desc'));
+	
+	if (limit === null) {
+		limit = 100;
+	}
+	if (offset === null) {
+		offset = 0;
+	}
+	
+	if(!entityLib.hasConflictingParameters(id, count, metadata)) {
+		// switch based on method type
+		if ((method === 'POST')) {
+			// create
+			exports.createSchool_users();
+		} else if ((method === 'GET')) {
+			// read
+			if (id) {
+				exports.readSchool_usersEntity(id);
+			} else if (count !== null) {
+				exports.countSchool_users();
+			} else if (metadata !== null) {
+				exports.metadataSchool_users();
+			} else {
+				exports.readSchool_usersList(limit, offset, sort, desc);
+			}
+		} else if ((method === 'PUT')) {
+			// update
+			exports.updateSchool_users();    
+		} else if ((method === 'DELETE')) {
+			// delete
+			if(entityLib.isInputParameterValid(idParameter)){
+				exports.deleteSchool_users(id);
+			}
+		} else {
+			entityLib.printError(javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST, 1, "Invalid HTTP Method");
+		}
+	}
+	
+	// flush and close the response
+	response.getWriter().flush();
+	response.getWriter().close();
+};
