c98d742a58d3375a9cbfd70ed5746a4b36c5c785
valchovski <bvalchovski@gmail.com>
2015-06-27 10:49:39 +0000

    Changes project structure

---
+++ Robosoft/ScriptingServices/Robosoft/principal/school_users.js
@@ -0,0 +1,3 @@
+var entitySchool_users = require('Robosoft/principal/school_users_lib');
+
+entitySchool_users.processSchool_users();
--- Robosoft/ScriptingServices/Robosoft/school_users.js
+++
@@ -1,3 +0,0 @@
-var entitySchool_users = require('Robosoft/school_users_lib');
-
-entitySchool_users.processSchool_users();
